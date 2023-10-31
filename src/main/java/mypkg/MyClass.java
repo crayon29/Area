f783aae274318ddd763a5872ba6f432eec412f6f
“crayon29” <swagat5foryou@gmail.com>
2023-10-31 23:29:11 +0530

    new tri added

--- src/main/java/mypkg/MyClass.java
+++ src/main/java/mypkg/MyClass.java
@@ -8,6 +8,11 @@ public class MyClass {
 	{
 		return (l*b);
 	}
+	
+	public double AreaTri(int r)
+	{
+		return (3.14*r*r);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
